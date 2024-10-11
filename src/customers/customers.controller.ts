dde34db32cfff2109c74a61ce1c65005d5cdd2c3
Éricles Miller <ericlesmiller15@gmail.com>
2024-10-11 15:20:56 -0300

    feat: insert header in swagger

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, Req, UseGuards } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
-import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 
 @ApiTags('Customers')
@@ -13,6 +13,12 @@ export class CustomersController {
   @ApiOperation({summary: 'Create a new customer with data in request'})
   @ApiProperty()
   @Post()
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   @HttpCode(201)
   create(@Body() createCustomerDto: CreateCustomerDto, @Req() req: Request) {
     const user = req.user;
@@ -20,11 +26,23 @@ export class CustomersController {
   }
 
   @Get()
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   findAll() {
     return this.customersService.findAll();
   }
 
   @Get(':id')
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   findOne(@Param('id') id: string) {
     return this.customersService.findOne(+id);
   }
--- src/proposal/proposal.controller.ts
+++ src/proposal/proposal.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, Req } from
 import { ProposalService } from './proposal.service';
 import { CreateProposalDto } from './dto/create-proposal.dto';
 import { UpdateProposalDto } from './dto/update-proposal.dto';
-import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { Proposal } from './entities/proposal.entity';
 import { User } from 'src/users/entities/user.entity';
@@ -13,6 +13,12 @@ export class ProposalController {
   constructor(private readonly proposalService: ProposalService) {}
 
   @Post() // ok
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   @HttpCode(201)
   @ApiOperation({summary: 'Create a new proposal with data in request'})
   @ApiProperty()
@@ -22,18 +28,36 @@ export class ProposalController {
   }
 
   @Get() // ok
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   async findAll(@Req() req: Request) : Promise<Proposal[]> {
     const user = req.user;
     return this.proposalService.findAll(user);
   }
 
   @Get('refused') //ok
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   async findAllRefused(@Req() req: Request): Promise<Proposal[]>{
     const user = req.user;
     return await this.proposalService.findAllRefused(user);
   }
 
   @Get(':id')
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   async getProposalById( 
     @Param('id') proposalId: number, @Req() req: { user: User },
   ): Promise<Proposal> {
@@ -43,6 +67,12 @@ export class ProposalController {
   
 
   @Patch(':id/approve') //ok
+  @ApiHeader({
+    name: 'user_id',
+    description: 'payload user',
+    required: true,
+    example: '1'
+  })
   async update(@Param('id') id: number, @Body() updateProposalDto: UpdateProposalDto) : Promise<Proposal> {
     return this.proposalService.update(+id, updateProposalDto);
   }
